5a2ae1ead2ae0225b46922762d5cf45f3df9355d
Rhon Dwyane <rhon.dwyane03@gmail.com>
2025-02-16 11:48:59 +0800

    Update

--- src/mainPages/AboutUs.jsx
+++ src/mainPages/AboutUs.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './AU.css';
 
 function AboutUs() {
   return (
-    <div>
-      About Us
-    </div>
-  )
+    <>
+      <header className='about-header'>
+        <div className="nav-container">
+          <h1 className='business-name'>Samgyeop Grill Box - Cebu</h1>
+          <nav>
+            <ul className='nav-list'>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/menu">Menu</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </nav>
+        </div>
+        <hr />
+        <h1 className='about-now-title'>About Us</h1>
+        <h2>Learn more about our mission, values, and the work we do.</h2>
+      </header>
+
+      <div className="about-container-1">
+      </div>
+    </>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
--- src/mainPages/Menu.jsx
+++ src/mainPages/Menu.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import './M.css';
 function Menu() {
   return (
-    <div>
-      MENU
-    </div>
+    <>
+      <header className='menu-header'>
+          <div className="nav-container">
+            <h1 className='business-name'>Samgyeop Grill Box - Cebu</h1>
+            <nav>
+              <ul className='nav-list'>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/menu">Menu</Link></li>
+                <li><Link to="/about">About</Link></li>
+              </ul>
+            </nav>
+          </div>
+          <hr />
+          <h1 className='menu-now-title'>Our Menu</h1>
+          <h2>Delicious Korean BBQ Selections, Fresh & Ready to Grill!</h2>
+        </header>
+        <div className="menu-container-1">
+
+        </div>
+      </>
   )
 }
 
